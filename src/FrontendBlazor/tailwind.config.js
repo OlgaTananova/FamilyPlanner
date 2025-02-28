b12edf773dce5165c0e900ed69b44c2c59567027
gitName <olga.tananova@gmail.com>
2025-02-27 16:25:08 -0800

    Added BlazorProject

---
+++ src/FrontendBlazor/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./**/*.razor",
+        "./Pages/**/*.razor",
+        "./Shared/**/*.razor",
+        "./wwwroot/index.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
